3f6d9f5d437b2112ad31cbf153f9b1b49e3c538c
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-01-09 02:09:26 +0000

    adaptations to new input/output helper classes to get things running again

--- test/fwliteWithPythonConfig_cfg.py
+++ test/fwliteWithPythonConfig_cfg.py
@@ -2,12 +2,18 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("FWLitePlots")
 
-process.MuonAnalyzer = cms.PSet(
-    ## common input for wrapped analyzers
-    fileNames   = cms.vstring('file:patTuple.root'),  ## mandatory
-    outputFile  = cms.string('analyzeFWLiteHistograms.root'),## mandatory
-    maxEvents   = cms.int32(-1),                      ## optional
-    outputEvery = cms.uint32(10),                     ## optional
+##process.fwliteParameters = cms.PSet(
+process.fwliteInput = cms.PSet(
+    fileNames   = cms.vstring('file:patTuple.root'),         ## mandatory
+    maxEvents   = cms.int32(-1),                             ## optional
+    outputEvery = cms.uint32(10),                            ## optional
+)
+    
+process.fwliteOutput = cms.PSet(
+    fileName  = cms.string('analyzeFWLiteHistograms.root'),## mandatory
+)
+
+process.muonAnalyzer = cms.PSet(
     ## input specific for this analyzer
     muons = cms.InputTag('cleanPatMuons')
 )
